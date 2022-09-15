0a0a7ccee98832d3448716f93bf118133d15906e
Darek <dariuszgrados@gmail.com>
2022-09-15 14:26:36 +0200

    Create Category Model

---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Data;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreatedDataTime { get; set; } = DateTime.Now;
+    }
+}
